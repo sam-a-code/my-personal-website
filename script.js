969733e20a51a96de628f8b13d22f8c66ee712cb
Sam Avery <sam.avery1@gmail.com>
2022-08-20 12:25:42 -0500

    copy changes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Old patterns and designs";
 document.querySelector("body").appendChild(h2);
+
+
